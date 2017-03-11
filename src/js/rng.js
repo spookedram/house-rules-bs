9685c55a02d44d3e3e1187b28bde210e4e174314
Zach Allen <spookedram@gmail.com>
2017-03-10 21:13:09 -0600

    added rng

---
+++ src/js/rng.js
@@ -0,0 +1,9 @@
+var rollBtn = document.getElementById("roll-btn");
+var rollResult = document.getElementById("roll-result");
+
+function rollDice(){
+  var min = Number(document.getElementById("roll-min").value);
+  var max = Number(document.getElementById("roll-max").value);
+
+  rollResult.innerHTML = Math.floor(Math.random() * (max - min + 1)) + min;
+}
