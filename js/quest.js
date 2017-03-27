f60f629ef36a0e9533b6e4b28ba5f87f3f56e2cd
Zach Allen <zallen@aetn.org>
2017-03-27 16:04:04 -0500

    area add and delete v1

--- js/quest.js
+++ js/quest.js
@@ -14,6 +14,8 @@ var npcsInput = document.getElementById("npcsInput");
 
 var saveLoadModal = new Modal(document.getElementById("modal-btn"), {});
 
+var currentArea;
+
 // returns characterList array from localStorage
 function getQuestList() {
   if(lsTest() === true) {
@@ -194,19 +196,6 @@ function clearItem(btn,pin) {
   btn.parentNode.parentNode.parentNode.parentNode.removeChild(btn.parentNode.parentNode.parentNode);
 }
 
-function addAnArea() {
-  var input = document.getElementById("settingsInput");
-  var tableBody = document.getElementById("settingsList");
-
-  var newHTML = "<tr><td><button type='button' class='btn btn-default no-print' onclick='deleteRow(this)'><span class='glyphicon glyphicon-remove'></span></button></td><td class='setting' style='vertical-align:middle'>" + input.value + "</td><td  class='text-right'><button type='button' class='btn btn-default no-print'>Load</button></td></tr>";
-
-  if(input.value !== "") {
-    tableBody.innerHTML += newHTML;
-    settingList.push(input.value);
-    input.value = "";
-  }
-}
-
 function deleteRow(btn) {
   btn.parentNode.parentNode.parentNode.removeChild(btn.parentNode.parentNode);
 }
@@ -221,8 +210,50 @@ function toggleBtnText(btn, target) {
   }
 }
 
-function addTrap() {
+function Area() {
+  this.pin = "";
+  this.name = "";
+  this.desc = "";
+  this.events = [];
+}
 
+function addAnArea() {
+  var area = new Area();
+  area.pin = uniqueNumber();
+  area.name = document.getElementById("areaName").value;
+  area.desc = document.getElementById("areaDesc").value;
+
+  var wrapper = document.getElementById("areaListWrapper");
+
+  var newHTML = '<div id=' + area.pin + ' class="area panel panel-default"><div class="panel-heading"><h4>' + area.name + '</h4></div><div class="panel-body"><div class="row"><div class="col-sm-6"><b>Description</b><p>' + area.desc + '</p></div><div class="col-sm-6 text-right no-print"><h4 style="margin-top:0;">Add an Event</h4><div class="btn-group"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#enemyModal">Enemy</button><button type="button" class="btn btn-default" data-toggle="modal" data-target="#trapModal">Trap</button><button type="button" class="btn btn-default" data-toggle="modal" data-target="#puzzleModal">Puzzle</button></div><div class="btn-group"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#npcModal">NPC</button><button type="button" class="btn btn-default" data-toggle="modal" data-target="#itemModal">Quest Item</button></div></div></div><h4>Events</h4><p id="area' + area.pin + 'eventsEmpty">You have not made any events for this area yet! Use the buttons on the right to create a new event.</p><div id="area' + area.pin + 'eventList" class="row equal"></div></div><div class="panel-footer text-right no-print"><button type="button" class="btn btn-danger" onclick="deleteArea(' + area.pin + ')">Delete Area</button></div></div>';
+
+
+  if(document.getElementById("areaName").value !== "" && document.getElementById("areaDesc").value !== "") {
+    wrapper.innerHTML += newHTML;
+    document.getElementById("areaName").value = "";
+    document.getElementById("areaDesc").value = "";
+  }
+}
+
+function deleteArea(pin) {
+  var el = document.getElementById(pin);
+  document.getElementById("areaListWrapper").removeChild(el);
+}
+
+function addTrap(id) {
+  var name = document.getElementById("trapName").value;
+  var desc = document.getElementById("trapDesc").value;
+  var save = document.getElementById("trapST").value;
+  var dmg = document.getElementById("trapDamage").value;
+  var modal = document.getElementById("trapModal");
+  var newHTML = "";
+
+  newHTML = '<div class="event col-xs-12 col-sm-4"><div class="panel panel-default"><div class="panel-heading"><div class="row"><div class="col-xs-8"><h4 style="margin:8px auto">Event</span></h4></div><div class="col-xs-4 text-right no-print"><button type="button" class="btn btn-danger delete-btn" onclick="deleteEnemy(this,)"><span class="glyphicon glyphicon-remove"></span></button></div></div></div><div class="panel-body"><p contenteditable="true"><b>' + name + '</b></p><p contenteditable="true">' + desc + '</p><p class="text-right n-mb">Saving Throw: ' + save + '<br>Damage: ' + dmg + '</p></div></div></div>';
+
+  if(name !== "" && desc !== "") {
+    addToInnerHTML(document.getElementById("area" + id + "eventList"), newHTML);
+    modal.hide();
+  }
 }
 
 function addPuzzle() {
@@ -234,5 +265,5 @@ function addNPCInteraction() {
 }
 
 function addQuestItem() {
-  
+
 }
