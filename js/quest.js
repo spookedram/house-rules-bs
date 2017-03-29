010051f9df58a4fa24801f7166ea16e278c07541
Zach Allen <zallen@aetn.org>
2017-03-29 15:23:27 -0500

    added url img uploading

--- js/quest.js
+++ js/quest.js
@@ -298,7 +298,7 @@ function addMap() {
     var map = new Map();
     map.name = document.getElementById("mapName").value;
 
-    var newHTML = '<div id=' + map.pin + ' class="map"><hr class="n-mb"><div class="row"><div class="col-xs-8"><h3 contenteditable="true">' + map.name + '</h3></div><div class="col-xs-4 text-right"><button type="button" class="btn btn-danger" style="margin:15px 0;" onclick="showModal(deleteMapModal), setCurrentArea(0, ' + map.pin + ')"><span class="glyphicon glyphicon-remove"></span></button></div></div><div class="no-print"><p>Upload an Image</p><input type="file" id="imageLoader' + map.pin + '" name="imageLoader"/><div class="text-center" style="margin-bottom:15px;"><canvas id="imageCanvas' + map.pin + '" height="0" class="img-responsive"></canvas></div></div><div id="map' + map.pin + 'images"></div><div id="map' + map.pin + 'areaList"></div><div class="row no-print"><div class="col-sm-6 col-sm-offset-3"><div class="input-group"><input type="text" id="area' + map.pin + 'Name" class="form-control newAreaInput" placeholder="Area Name" onkeypress="addAreaEnter(event,' + map.pin + ')"><div class="input-group-btn"><button type="button" class="btn btn-default" onclick="addAnArea(' + map.pin + ')">Add</button></div></div></div></div></div>';
+    var newHTML = '<div id=' + map.pin + ' class="map"><hr class="n-mb"><div class="row"><div class="col-xs-8"><h3 contenteditable="true">' + map.name + '</h3></div><div class="col-xs-4 text-right"><button type="button" class="btn btn-danger" style="margin:15px 0;" onclick="showModal(deleteMapModal), setCurrentArea(0, ' + map.pin + ')"><span class="glyphicon glyphicon-remove"></span></button></div></div><div class="no-print"><p>Upload an Image<br><i>Warning: Saving images to your map takes up a LOT of space in your localStorage. Upload by URL when you can to save space.</i></p><input type="file" id="imageLoader' + map.pin + '" name="imageLoader"/><div class="input-group col-sm-6" style="margin-top:15px; margin-bottom:15px;"><input id="imageURL' + map.pin + '" type="text" class="form-control" placeholder="Paste URL here"><div class="input-group-btn"><button type="button" class="btn btn-primary" onclick="addImage(' + map.pin + ')">Upload</button></div></div><div class="text-center"><canvas id="imageCanvas' + map.pin + '" height="0" style="margin-bottom:15px;" class="img-responsive"></canvas></div></div><div id="map' + map.pin + 'areaList"></div><div class="row no-print"><div class="col-sm-6 col-sm-offset-3"><div class="input-group"><input type="text" id="area' + map.pin + 'Name" class="form-control newAreaInput" placeholder="Area Name" onkeypress="addAreaEnter(event,' + map.pin + ')"><div class="input-group-btn"><button type="button" class="btn btn-default" onclick="addAnArea(' + map.pin + ')">Add</button></div></div></div></div></div>';
 
     mapList.push(map);
     document.getElementById("mapList").innerHTML += newHTML;
@@ -317,6 +317,23 @@ document.getElementById("mapName").onkeypress = function(e){
     return false;
   }
 };
+
+function addImage(mapId) {
+  var map = getPinFromArray(mapList, mapId);
+  var imageLoader = document.getElementById('imageURL' + mapId);
+  var canvas = document.getElementById('imageCanvas' + mapId);
+  var ctx = canvas.getContext('2d');
+
+  var img = new Image();
+  img.onload = function(){
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img,0,0);
+  };
+  img.src = imageLoader.value;
+  img.id = "map" + mapId + "img";
+  map.img = img.src;
+}
 
 function uploadImage(mapId) {
   var map = getPinFromArray(mapList, mapId);
