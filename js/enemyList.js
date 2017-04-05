32369483ad61260c76889a04413f1865b0434291
Zach Allen <zallen@aetn.org>
2017-04-05 11:28:46 -0500

    fixed damage dice on enemy creator

--- js/enemyList.js
+++ js/enemyList.js
@@ -97,32 +97,32 @@ function clearEnemyCreator() {
 function getLvlDmgBonus() {
   switch(document.getElementById("lvl").value) {
     default:
-      return 1;
     case "2":
+      return 1;
     case "3":
-      return 2;
     case "4":
     case "5":
+      return 2;
     case "6":
-      return 3;
     case "7":
     case "8":
+      return 3;
     case "9":
-      return 4;
     case "10":
     case "11":
+      return 4;
     case "12":
-      return 5;
     case "13":
     case "14":
+      return 5;
     case "15":
-      return 6;
     case "16":
     case "17":
+      return 6;
     case "18":
-      return 7;
     case "19":
     case "20":
+      return 7;
   }
 }
 
